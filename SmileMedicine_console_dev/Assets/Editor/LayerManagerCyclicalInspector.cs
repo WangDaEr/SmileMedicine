47b92f452d5ec6024116292e72a6f34f346c5458
王元鹏 <985801851@qq.com>
2019-06-18 23:15:24 +0800

    done:     1.generate linear and cyclical layers dynamically.     2.customized design tool need:     1.merge with general layer system.     2.cyclical layer transformation control.

--- SmileMedicine_console_dev/Assets/Editor/LayerManagerCyclicalInspector.cs
+++ SmileMedicine_console_dev/Assets/Editor/LayerManagerCyclicalInspector.cs
@@ -7,27 +7,38 @@ using UnityEditor;
 [CustomEditor(typeof(LayerManagerCyclical))]
 public class LayerManagerCyclicalInspector : Editor
 {
-    /*
-    // Start is called before the first frame update
-    void Start()
+    public enum LayerTransformationOptions
     {
-        
+        OneDirection,
+        Cyclical
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    */
     LayerManagerCyclical lmc;
 
     private int layersNumber = 0;
+    public int LayersNumber { get { return layersNumber; } set { layersNumber = value; lmc.LayersNumber = value; } }
+
+    private int layerIndex_init = 0;
+    public int LayerIndex_init { get { return layerIndex_init; } set { layerIndex_init = value; lmc.layerIndex_init = value; } }
+
+    private LayerTransformationOptions layerTranformationOption = LayerTransformationOptions.OneDirection;
+
+    private float y_Offset = 0.0F;
+    public float Y_Offset { get { return y_Offset; } set { y_Offset = value; lmc.Y_Offset = value; } }
+
+    private float z_Offset = 0.0F;
+    public float Z_Offset { get { return z_Offset; } set { z_Offset = value; lmc.Z_Offset = value; } }
+
+    private float cylinderRadius = 0.0F;
+    public float CylinderRadius { get { return cylinderRadius; } set { cylinderRadius = value; lmc.CylinderRadius = value; } }
+
+    private float cylinderIntervalDegree = 0.0F;
+    public float CylinderIntervalDegree { get { return cylinderIntervalDegree; } set { cylinderIntervalDegree = value; lmc.CylinderIntervalDegree = value; } }
 
     void OnEnable()
     {
         lmc = (LayerManagerCyclical)target;
-        layersNumber = lmc.layers.Count;
+        layersNumber = lmc.transform.childCount;
     }
 
     private void CheckLayerNumber(int newLayerNum)
@@ -36,48 +47,146 @@ public class LayerManagerCyclicalInspector : Editor
         {
             layersNumber = newLayerNum;
 
-            foreach (GameObject go in lmc.layers)
-            {
-                DestroyImmediate(go);
-            }
-
-            lmc.layers = new List<GameObject>();
+            lmc.layers.Clear();
             for (int i = 0; i < layersNumber; ++i)
             {
                 //GameObject placeHolder = new GameObject();
-                //placeHolder.transform.SetParent(lmc.transform, true);
                 lmc.layers.Add(null);
+                //GameObject newLayer = new GameObject();
+                //newLayer.transform.SetParent(lmc.transform);
                 //lmc.layers[i] = (GameObject)EditorGUILayout.ObjectField("Layer " + i, lmc.layers[i], typeof(GameObject), false);
             }
+            
         }
 
         
     }
 
+    private void CheckLayer_T_Option(LayerTransformationOptions newOption)
+    {
+        if (layerTranformationOption != newOption)
+        {
+            layerTranformationOption = newOption;
+
+            DestroyLayers();
+        }
+    }
+
+    private void DestroyLayers()
+    {
+        while (lmc.transform.childCount > 0)
+        {
+            DestroyImmediate(lmc.transform.GetChild(0).gameObject);
+        }
+    }
+
+    private void InstantiateLayers()
+    {
+        bool isOneDir = layerTranformationOption == LayerTransformationOptions.OneDirection;
+
+        for (int i = 0; i < lmc.layers.Count; ++i)
+        {
+            float index_diff = i - LayerIndex_init;
+
+            Vector3 newTranslation = new Vector3(
+                0.0F,
+                isOneDir ? index_diff * -Y_Offset : 0.0F,
+                isOneDir ? index_diff * Z_Offset : CylinderRadius
+                );
+            /*
+            Vector3 newRot_v3 = new Vector3(
+                isOneDir ? 0.0F : index_diff * CylinderIntervalDegree,
+                0.0F,
+                0.0F
+                );
+            */
+
+            GameObject newObj = Instantiate(lmc.layers[i], lmc.transform);
+
+            newObj.transform.position = new Vector3(0.0F, 0.0F, 0.0F);
+            newObj.transform.Rotate(Vector3.right, index_diff * CylinderIntervalDegree, Space.World);
+            newObj.transform.Translate(newTranslation, Space.Self);
+
+            
+            if (!isOneDir)
+            {
+                newObj.transform.Translate(new Vector3(newTranslation.x, -newTranslation.z, 0.0F), Space.World);
+            }
+        }
+    }
+
+    private void UpdateLayersPosition()
+    {
+        bool isOneDir = layerTranformationOption == LayerTransformationOptions.OneDirection;
+        for (int i = 0; i < lmc.transform.childCount; ++i)
+        {
+            float index_diff = i - LayerIndex_init;
+            Transform layer = lmc.transform.GetChild(i);
+
+            Vector3 newTranslation = new Vector3(
+                0.0F,
+                isOneDir ? index_diff * -Y_Offset : 0.0F,
+                isOneDir ? index_diff * Z_Offset : CylinderRadius
+                );
+
+            
+            layer.transform.rotation = Quaternion.Euler(new Vector3(-90.0F, 0.0F, 0.0F));
+            layer.transform.position = new Vector3(0.0F, 0.0F, 0.0F);
+
+            layer.transform.Rotate(Vector3.right, index_diff * CylinderIntervalDegree, Space.World);
+            layer.transform.Translate(newTranslation, Space.Self);
+
+            if (!isOneDir)
+            {
+                layer.transform.Translate(new Vector3(newTranslation.x, -newTranslation.z, 0.0F), Space.World);
+            }
+        }
+    }
+
     public override void OnInspectorGUI()
     {
-        //base.OnInspectorGUI();
-
         EditorGUILayout.BeginVertical();
 
         CheckLayerNumber(EditorGUILayout.IntSlider("Number of Layers", layersNumber, 0, 10));
 
-        if (layersNumber < 5)
+        LayerIndex_init = EditorGUILayout.IntSlider("Index of Initial Layer", LayerIndex_init, 0, layersNumber);
+
+        //layerTranformationOption = (LayerTransformationOptions)EditorGUILayout.EnumPopup("Layer Transformation Option", layerTranformationOption);
+        CheckLayer_T_Option((LayerTransformationOptions)EditorGUILayout.EnumPopup("Layer Transformation Option", layerTranformationOption));
+
+        if (layerTranformationOption == LayerTransformationOptions.OneDirection)
         {
-            EditorGUILayout.HelpBox("12345", MessageType.Warning);
+            Y_Offset = EditorGUILayout.Slider("Depth", Y_Offset, 0, 15);
+            Z_Offset = EditorGUILayout.Slider("Height", Z_Offset, 0, 15);
         }
-        else
+        else if (layerTranformationOption == LayerTransformationOptions.Cyclical)
         {
-            EditorGUILayout.HelpBox("678910", MessageType.Warning);
+            CylinderRadius = EditorGUILayout.Slider("Cylinder Radius", CylinderRadius, 0, 15);
+            CylinderIntervalDegree = EditorGUILayout.Slider("Interval Degree", CylinderIntervalDegree, 0, layersNumber > 0 ? 360 / layersNumber : 180);
         }
 
-        
-        for (int i = 0; i < lmc.layers.Count; ++i)
+
+        for (int i = 0; i < layersNumber; ++i)
         {
             Debug.Log("length: " + lmc.layers.Count + " " + i + " " + layersNumber);
             lmc.layers[i] = (GameObject)EditorGUILayout.ObjectField("Layer " + i, lmc.layers[i], typeof(GameObject), false);
         }
-        
+
+        if (GUILayout.Button("Instantiate All Layers"))
+        {
+            InstantiateLayers();
+        }
+
+        if (GUILayout.Button("Update Positions of All Layers"))
+        {
+            UpdateLayersPosition();
+        }
+
+        if (GUILayout.Button("Destroy All Layers"))
+        {
+            DestroyLayers();
+        }
+
         EditorGUILayout.EndVertical();
     }
 }
--- SmileMedicine_console_dev/Assets/Scripts/CyclicalLayerSystem/LayerManagerCyclical.cs
+++ SmileMedicine_console_dev/Assets/Scripts/CyclicalLayerSystem/LayerManagerCyclical.cs
@@ -8,6 +8,16 @@ public class LayerManagerCyclical : MonoBehaviour
     public List<GameObject> layers = new List<GameObject>();
     public int LayersNumber;
 
+    public int layerIndex_init = 0;
+
+    public int layerTranformationOption = 0;
+
+    public float Y_Offset;
+    public float Z_Offset;
+
+    public float CylinderRadius;
+    public float CylinderIntervalDegree;
+
     private void InstantiateLayers()
     {
         foreach (GameObject layer in layers)
@@ -19,7 +29,7 @@ public class LayerManagerCyclical : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InstantiateLayers();
+        //InstantiateLayers();
     }
 
     // Update is called once per frame
